e0e30f5f2fdb9d269942082c1d96324bfb2bfa68
Irina Bobina <irsen-bia@mail.ru>
2022-10-25 01:02:21 +0500

    Initial Commit

---
+++ lesson_2_1_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson_2_1_1
+//
+//  Created by Ирина on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
